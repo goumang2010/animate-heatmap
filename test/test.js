6f7105d12e155eb8d42e14c9a58379a4facec846
goumang2010 <goumang2010@live.com>
2017-05-05 23:57:20 +0800

    groupPoints test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import chaiPromised from 'chai-as-promised';
 import Heatmap from '../src';
+import * as utils from '../src/utils';
 chai.use(chaiPromised);
 const expect = chai.expect;
 
@@ -7,6 +8,14 @@ describe('Heatmap instance', function() {
     it('should find inject script in html', function(done) {
         let heatmap = new Heatmap();
         expect(heatmap.option).to.exist;
+        done();
+    });
+});
+
+describe('utils', function() {
+    it('groupPoints should work', function(done) {
+        let result = utils.groupPoints([[1,2], [3,4], [101,102],[103,104]], 10)
+        expect(result.length).to.equal(2);
         done();
     });
 });
