a4383255a64cbe59f6e01a6789d23e25a604fabd
goumang2010 <goumang2010@live.com>
2017-05-09 10:19:16 +0800

    remove example

--- examples/app.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import Heatmap from './heatmap.vue';
-
-/* eslint-disable no-new */
-const component = new Vue({...Heatmap}).$mount();
-document.getElementsByTagName('body')[0].appendChild(component.$el);
